ab127dcdc0e61f809451ea7f7023b33f04b26f5c
ZINO <zino@grampus.co>
2023-09-26 15:53:16 +0900

    dev commit

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-const app = require("express");
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view_engine", "ejs");
+app.use(express.static("public"));
+
+app.listen(80, function(){
+    console.log("START");
+});
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+});
 
